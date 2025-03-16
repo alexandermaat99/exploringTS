0bb3c14a2fa8edeafdfe74d7f5b1bff342db55cb
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2025-03-16 15:14:20 -0700

    card components for every item in notes table

--- exploring/app/notes/page.tsx
+++ exploring/app/notes/page.tsx
@@ -2,8 +2,13 @@
 
 import { createClient } from "@/utils/supabase/client";
 import { useEffect, useState } from "react";
+import NoteCard from "@/components/card/note-card";
 
 export default function Page() {
+  //creates a state variable called notes, and a function to update it called setNotes
+  // after the equals sign says notes can be either an array of any, or type null, this is inside the <>
+  // inside the parameters is the value we're initializing notes to null, we could also initialize it an array []
+
   const [notes, setNotes] = useState<any[] | null>(null);
   const supabase = createClient();
 
@@ -15,5 +20,17 @@ export default function Page() {
     getData();
   }, []);
 
-  return <pre>{JSON.stringify(notes, null, 2)}</pre>;
+  return (
+    <div className="card-container">
+      {notes &&
+        notes.map((note) => (
+          <NoteCard
+            key={note.id}
+            id={note.id}
+            title={note.title}
+            content={note.text}
+          />
+        ))}
+    </div>
+  );
 }
