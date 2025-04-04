35f38746b349a4dd0b7461e2052658db77d11382
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2025-04-03 21:54:11 -0600

    testing more dark mode styles

--- exploring/app/protected/records/page.tsx
+++ exploring/app/protected/records/page.tsx
@@ -101,7 +101,7 @@ export default async function RecordsPage() {
       {records && records.length > 0 ? (
         <div className="overflow-x-auto w-full">
           <table className="min-w-full divide-y divide-gray-200 shadow-sm">
-            <thead className="bg-gray-50">
+            <thead className="bg-gray-50 dark:bg-slate-700">
               <tr>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Driver
