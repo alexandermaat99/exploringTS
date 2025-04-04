d55816bca498ba5e231fd3442fb1fdb0b97c80c5
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2025-04-03 22:22:13 -0600

    new width

--- exploring/app/layout.tsx
+++ exploring/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
           <main className="min-h-screen flex flex-col items-center">
             <div className="flex-1 w-full flex flex-col gap-5 items-center">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+                <div className="w-full max-w-8xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
                     {/* <Link href={"/"}>Next.js Supabase Starter</Link> */}
                     <Link href={"/"}>Track Records</Link>
@@ -51,7 +51,7 @@ export default function RootLayout({
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
               </nav>
-              <div className="flex flex-col gap-20 max-w-6xl p-5">
+              <div className="flex flex-col gap-20 max-w-8xl p-5">
                 {children}
               </div>
 
