1041d897a32da1334d2679a4506e802131393840
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2025-04-03 21:55:49 -0600

    more dm styles

--- exploring/app/protected/records/page.tsx
+++ exploring/app/protected/records/page.tsx
@@ -101,7 +101,7 @@ export default async function RecordsPage() {
       {records && records.length > 0 ? (
         <div className="overflow-x-auto w-full">
           <table className="min-w-full divide-y divide-gray-200 shadow-sm">
-            <thead className="bg-gray-50 dark:bg-slate-700">
+            <thead className="bg-gray-50 dark:bg-slate-400">
               <tr>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Driver
@@ -123,7 +123,7 @@ export default async function RecordsPage() {
                 </th>
               </tr>
             </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
+            <tbody className="bg-white divide-y dark:bg-slate-700 divide-gray-200">
               {records.map((record) => (
                 <tr key={record.id} className="hover:bg-gray-50">
                   <td className="px-6 py-4 whitespace-nowrap">
